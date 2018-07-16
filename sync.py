72aa9605d1d6301e3cbcb969528835bd3a636df6
agriks <kdstratton@gmail.com>
2018-07-16 19:49:47 -0400

    Changed most string literals to constants

--- sync.py
+++ sync.py
@@ -3,6 +3,20 @@ from typing import List, Callable
 from threading import Timer
 from collections import OrderedDict, Counter
 
+FAILED_MSG = "Sync failed!"
+NEED_MORE_MSG = "You need more people to sync!"
+START_MSG = "Buckle up syncers!"
+INVALID_MSG = "Invalid syncer!"
+WAIT_MSG = "Wait for the current sync to finish!"
+NO_SYNC_MSG = "There is no sync!"
+NOT_IN_MSG = "You are not in the current sync!"
+NOT_IN_PREV_MSG = "You were not in the last sync!"
+DESYNC_MSG = "Desyncing..."
+FEW_PPL_GROUP_MSG = "You need more people to make a group!"
+NAME_TAKEN_MSG = "There is a already a group with that name!"
+GROUP_ADD_MSG = "Group added!"
+NOT_IN_GROUP_MSG = "You aren't in that group!"
+GROUP_DNE_MSG = "That group does not exist!"
 
 old_syncs = []
 current_sync = None
@@ -72,7 +86,7 @@ def end_sync() -> None:
 
 def fail_sync(bot_msg: Callable[[str], None]) -> None:
   end_sync()
-  bot_msg("Sync failed!")
+  bot_msg(FAILED_MSG)
 
 
 def store_old_sync(sync: Sync) -> None:
@@ -112,31 +126,31 @@ def start_sync(starter: str, syncers: List[str], channel_users: List[str],
       current_sync = Sync(prepare_syncer_list(starter, syncers))
       if len(current_sync.syncers) <= 1:
         current_sync = None
-        bot_msg("You need more people to sync!")
+        bot_msg(NEED_MORE_MSG)
         return
-      bot_msg("Buckle up syncers!")
+      bot_msg(START_MSG)
       sync_timer = Timer(timer_time, lambda: fail_sync(bot_msg))
       sync_timer.start()
     else:
-      bot_msg("Invalid syncer!")
+      bot_msg(INVALID_MSG)
   else:
-    bot_msg("Wait for the current sync to finish!")
+    bot_msg(WAIT_MSG)
 
 
 def ready_syncer(syncer: str, bot_msg: Callable[[str], None]) -> None:
   if current_sync is None:
-    bot_msg("There is no sync!")
+    bot_msg(NO_SYNC_MSG)
   else:
     if current_sync.ready(syncer):
       if current_sync.check_ready():
         commence_sync(bot_msg)
     else:
-      bot_msg("You are not in the current sync!")
+      bot_msg(NOT_IN_MSG)
 
 
 def resync(starter: str, channel_users: List[str], bot_msg: Callable[[str], None]) -> None:
   if current_sync is not None:
-    bot_msg("Wait for the current sync to finish!")
+    bot_msg(WAIT_MSG)
   else:
     syncers = [s.name for s in old_syncs[-1].syncers]
     contains_syncer = False
@@ -147,18 +161,18 @@ def resync(starter: str, channel_users: List[str], bot_msg: Callable[[str], None
     if contains_syncer:
       start_sync(starter, syncers, channel_users, bot_msg)
     else:
-      bot_msg("You were not in the last sync!")
+      bot_msg(NOT_IN_PREV_MSG)
 
 
 def desync(caller: str, bot_msg: Callable[[str], None]) -> None:
   if current_sync is None:
-    bot_msg("There is no sync!")
+    bot_msg(NO_SYNC_MSG)
     return
   if caller in [s.name.lower() for s in current_sync.syncers]:
-    bot_msg("Desyncing...")
+    bot_msg(DESYNC_MSG)
     end_sync()
   else:
-    bot_msg("You are not in the current sync!")
+    bot_msg(NOT_IN_MSG)
 
 
 def create_sync_group(starter: str, name: str, syncers: List[str],
@@ -166,19 +180,19 @@ def create_sync_group(starter: str, name: str, syncers: List[str],
                       bot_msg: Callable[[str], None]) -> None:
   s = prepare_syncer_list(starter, syncers)
   if not check_if_valid(s, channel_users):
-    bot_msg("Invalid syncer!")
+    bot_msg(INVALID_MSG)
     return
   if len(s) <= 1:
-    bot_msg("You need more people to make a group!")
+    bot_msg(FEW_PPL_GROUP_MSG)
     return
   if name.lower() in sync_groups.keys():
-    bot_msg("There is a already a group with that name!")
+    bot_msg(NAME_TAKEN_MSG)
   else:
     sync_groups[name.lower()] = s
     if len(sync_groups) > group_limit:
       r = sync_groups.popitem(False)
       bot_msg("Removing group '%s' because theres too many groups!" % r[0])
-    bot_msg("Group added!")
+    bot_msg(GROUP_ADD_MSG)
 
 
 def start_sync_by_group(starter: str, group: str, channel_users: List[str],
@@ -189,11 +203,24 @@ def start_sync_by_group(starter: str, group: str, channel_users: List[str],
       start_sync(starter, list(sync_groups[g]), channel_users, bot_msg)
       sync_groups.move_to_end(g)
     else:
-      bot_msg("You aren't in that group!")
+      bot_msg(NOT_IN_GROUP_MSG)
   else:
-    bot_msg("That group does not exist!")
+    bot_msg(GROUP_DNE_MSG)
 
 
 #endregion
 
+"""
 create_sync_group("agri", "team", ["agri", "dino"], ["agri","Dino"], print)
+start_sync("agri", ["agri", "dino"], ["agri", "dino"], print)
+desync("agri", print)
+resync("agri", ["agri", "dino"], print)
+desync("agri", print)
+start_sync_by_group("agri", "team", ["agri", "dino"], print)
+desync("agri", print)
+start_sync("agri", ["agri"], ["agri"], print)
+desync("agri", print)
+start_sync("agri", ["agri", "dino"], ["agri", "dino"], print)
+ready_syncer("agri", print)
+ready_syncer("dino", print)
+"""
